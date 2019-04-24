f867e0adac12cf05fa52f7f293f84e1eb1a35867
Amit Kumar Jaiswal <amitkumarj441@gmail.com>
2019-04-25 00:07:58 +0100

    Update pneumonia.py

--- stage_1/scripts/pneumonia.py
+++ stage_1/scripts/pneumonia.py
@@ -1,3 +1,4 @@
+
 import sys
 import os
 import pydicom
@@ -5,27 +6,27 @@ import numpy as np
 import cv2
 
 MODEL_DIR = '../model/Mask_RCNN'
-sys.path.append(os.path.join(MODEL_DIR))  
+sys.path.append(os.path.join(MODEL_DIR))  # To find local version of the library
 
 from mrcnn import utils
 from mrcnn.config import Config
 
 class PneumoniaConfig(Config):
-    """Configuration for training pneumonia detection on the RSNA pneumonia dataset.
-    Overrides values in the base Config class.
+    """Setting up configuration for training identification model.
+    Overrides values in the base config class.
     """
     
-    # configuration  
+    # Give the configuration a recognizable name  
     NAME = 'pneumonia'
     
-    # Training on 1 GPU and 8 images per GPU. Putting multiple images on each
+    # Train on 1 GPU and 8 images per GPU. We can put multiple images on each
     # GPU because the images are small. Batch size is 8 (GPUs * images/GPU).
     GPU_COUNT = 1
     IMAGES_PER_GPU = 8
     
-    BACKBONE = 'resnet50'
+    BACKBONE = 'resnet101'
     
-    NUM_CLASSES = 2  # background + 1 pneumonia classes
+    NUM_CLASSES = 2  # background + 1 pneumonia class
     
     IMAGE_MIN_DIM = 512
     IMAGE_MAX_DIM = 512
@@ -33,13 +34,46 @@ class PneumoniaConfig(Config):
     TRAIN_ROIS_PER_IMAGE = 32
     MAX_GT_INSTANCES = 3
     DETECTION_MAX_INSTANCES = 3
-    DETECTION_MIN_CONFIDENCE = 0.7  ## match target distribution
+    DETECTION_MIN_CONFIDENCE = 0.8  ## match target distribution
     DETECTION_NMS_THRESHOLD = 0.1
 
-    STEPS_PER_EPOCH = 400
+    STEPS_PER_EPOCH = 500
+    VALIDATION_STEPS = 200
+
+class PneumoniaConfig2(Config):
+    """Configuration for training pneumonia detection on the RSNA pneumonia dataset.
+    Overrides values in the base Config class.
+    """
+    
+    # Give the configuration a recognizable name  
+    NAME = 'pneumonia'
+    
+    # Train on 1 GPU and 8 images per GPU. We can put multiple images on each
+    # GPU because the images are small. Batch size is 8 (GPUs * images/GPU).
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 8
+    
+    BACKBONE = 'resnet101'
+    
+    NUM_CLASSES = 3  # background + 1 pneumonia class + 1 false positive class
+    
+    IMAGE_MIN_DIM = 512
+    IMAGE_MAX_DIM = 512
+    RPN_ANCHOR_SCALES = (32, 64, 128, 256)
+    TRAIN_ROIS_PER_IMAGE = 32
+    MAX_GT_INSTANCES = 3
+    DETECTION_MAX_INSTANCES = 3
+    DETECTION_MIN_CONFIDENCE = 0.6  ## match target distribution
+    DETECTION_NMS_THRESHOLD = 0.1
+
+    STEPS_PER_EPOCH = 500
     VALIDATION_STEPS = 200
 
 class InferenceConfig(PneumoniaConfig):
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+    
+class InferenceConfig2(PneumoniaConfig2):
     GPU_COUNT = 1
     IMAGES_PER_GPU = 1
 
@@ -50,7 +84,7 @@ class PneumoniaDataset(utils.Dataset):
     def __init__(self, image_fps, image_annotations, orig_height, orig_width):
         super().__init__(self)
         
-        # Add classes
+        # add classes
         self.add_class('pneumonia', 1, 'Lung Opacity')
         
         # add images 
@@ -93,4 +127,54 @@ class PneumoniaDataset(utils.Dataset):
                     cv2.rectangle(mask_instance, (x, y), (x+w, y+h), 255, -1)
                     mask[:, :, i] = mask_instance
                     class_ids[i] = 1
+        return mask.astype(np.bool), class_ids.astype(np.int32)
+
+class PneumoniaDataset2(utils.Dataset):
+    """Dataset class for training pneumonia detection on the RSNA pneumonia dataset.
+    """
+
+    def __init__(self, image_fps, image_annotations, orig_height, orig_width):
+        super().__init__(self)
+        
+        # add classes
+        self.add_class('pneumonia', 1, 'Lung Opacity')
+        self.add_class('pneumonia', 2, 'False Positive')
+        
+        # add images 
+        for i, fp in enumerate(image_fps):
+            annotations = image_annotations[fp]
+            self.add_image('pneumonia', image_id=i, path=fp, 
+                           annotations=annotations, orig_height=orig_height, orig_width=orig_width)
+            
+    def image_reference(self, image_id):
+        info = self.image_info[image_id]
+        return info['path']
+
+    def load_image(self, image_id):
+        info = self.image_info[image_id]
+        fp = info['path']
+        ds = pydicom.read_file(fp)
+        image = ds.pixel_array
+        # If grayscale. Convert to RGB for consistency.
+        if len(image.shape) != 3 or image.shape[2] != 3:
+            image = np.stack((image,) * 3, -1)
+        return image
+
+    def load_mask(self, image_id):
+        info = self.image_info[image_id]
+        annotations = info['annotations']
+        count = len(annotations)
+
+        mask = np.zeros((info['orig_height'], info['orig_width'], count), dtype=np.uint8)
+        class_ids = np.zeros((count,), dtype=np.int32)
+        for i, a in enumerate(annotations):
+            if a['class'] > 0:
+                x = int(a['x'])
+                y = int(a['y'])
+                w = int(a['width'])
+                h = int(a['height'])
+                mask_instance = mask[:, :, i].copy()
+                cv2.rectangle(mask_instance, (x, y), (x+w, y+h), 255, -1)
+                mask[:, :, i] = mask_instance
+                class_ids[i] = int(a['class'])
         return mask.astype(np.bool), class_ids.astype(np.int32)
